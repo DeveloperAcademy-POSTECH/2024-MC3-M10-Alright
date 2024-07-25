4cdb384398901c394414bfe1a05dfa293aab8e84
Xerath <yoondj98@naver.com>
2024-07-25 11:18:13 +0900

    init Project

---
+++ SipGuMan/SipGuMan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SipGuMan
+//
+//  Created by 윤동주 on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SipGuMan/SipGuMan/SipGuManApp.swift
@@ -0,0 +1,17 @@
+//
+//  SipGuManApp.swift
+//  SipGuMan
+//
+//  Created by 윤동주 on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SipGuManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
