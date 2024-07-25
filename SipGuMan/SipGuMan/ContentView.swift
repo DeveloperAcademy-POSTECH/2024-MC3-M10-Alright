5f73d8d9a0c2ff5ff6e03344c985fafb5b1db048
SANGDOLEE <xkdxh625@naver.com>
2024-07-25 15:09:15 +0900

    Feat: MainView에 Title, Button 생성

--- SipGuMan/SipGuMan/ContentView.swift
+++ SipGuMan/SipGuMan/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("현재 어떤 상황이세요?")
+                    .font(.largeTitle)
+                    .padding(.top, 50)
+                
+                Spacer()
+                
+                Button {
+                    print("피드백 시각화화면으로 이동")
+                } label: {
+                    Text("1:1 상황")
+                        .frame(maxWidth: .infinity, maxHeight: 50)
+                        .background(Color.yellow)
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 40)
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
