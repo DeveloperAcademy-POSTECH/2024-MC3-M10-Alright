122da7fe609bcd41eaff37d4ed57c17aeda52bf9
SANGDOLEE <xkdxh625@naver.com>
2024-08-06 16:31:48 +0900

    Design: Dynamic Island Expanded & LockScreen 디자인 변경

--- Alright/DyanmicIsland/DyanmicIslandLiveActivity.swift
+++ Alright/DyanmicIsland/DyanmicIslandLiveActivity.swift
@@ -35,6 +35,27 @@ struct DynamicIslandWidgetLiveActivity: Widget {
                 .padding([.top, .horizontal])
                 
                 HStack(spacing: 10) {
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 24)
+                            .fill(Color.clear)
+                            .frame(width: 95, height: 48)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 24)
+                                    .stroke(context.state.noiseLevel.noiseBorderGradientColor, lineWidth: 1)
+                            )
+                        HStack(alignment: .bottom, spacing: 1) {
+                            Text("\(context.state.noiseLevel.emoji)")
+                                .padding(.bottom, 2)
+                            Text("\(context.state.decibels)")
+                                .foregroundStyle(.sgmWhite)
+                                .font(.system(size: 22, weight: .semibold))
+                            Text("dB")
+                                .foregroundStyle(.sgmWhite)
+                                .font(.system(size: 16, weight: .regular))
+                                .padding(.bottom, 2)
+                        }
+                    }
                     ZStack(alignment: .leading) {
                         RoundedRectangle(cornerRadius: 24)
                             .fill(Color.gray.opacity(0.3))
@@ -44,24 +65,6 @@ struct DynamicIslandWidgetLiveActivity: Widget {
                             .fill(context.state.noiseLevel.noiseGradientColor)
                             .frame(width: CGFloat(context.state.progress) / 100 * 222, height: 48)
                     }
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 24)
-                            .fill(Color.clear)
-                            .frame(width: 95, height: 48)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 24)
-                                    .stroke(context.state.noiseLevel.noiseBorderGradientColor, lineWidth: 1)
-                            )
-                        HStack(spacing: 1) {
-                            Text("\(context.state.noiseLevel.emoji)")
-                            Text("\(context.state.decibels)")
-                                .foregroundStyle(.sgmWhite)
-                                .font(.system(size: 22, weight: .semibold))
-                            Text("dB")
-                                .foregroundStyle(.sgmWhite)
-                                .font(.system(size: 16, weight: .regular))
-                        }
-                    }
                 }
                 .padding([.bottom, .horizontal])
             }
@@ -69,45 +72,9 @@ struct DynamicIslandWidgetLiveActivity: Widget {
         } dynamicIsland: { context in
             DynamicIsland {
                 // Expanded Mode에서 나타날 View 구현
-                // Dynamice Island 우측에 표시되는 영역
+                // Dynamice Island 좌측에 표시되는 영역
                 DynamicIslandExpandedRegion(.leading) {
-                        VStack(alignment: .trailing) {
-                            Button(intent: EndLiveActivityIntent()) {
-                                Image(systemName: "xmark")
-                                    .resizable()
-                                    .frame(width: 16, height: 16)
-                                    .padding(10)
-                            }
-                            .padding(.leading)
-                            //                        Text("Alright")
-                            //                            .foregroundStyle(.sgmWhite)
-                            //                            .font(.system(size: 12, weight: .semibold))
-                            //                            .padding(.leading)
-                        }
-                    
-                }
-                
-                // Dynamic Island 좌측에 표시되는 영역
-                DynamicIslandExpandedRegion(.trailing) { }
-                
-                // Dynamic Island 바로 하단에 표시되는 영역
-                DynamicIslandExpandedRegion(.center) { }
-                
-                // Dynamic Island center 하단에 표시되는 영역
-                // center가 없을 시 center에 위치
-                DynamicIslandExpandedRegion(.bottom) {
                     HStack(alignment: .bottom) {
-                        ZStack(alignment: .leading) {
-                            RoundedRectangle(cornerRadius: 24)
-                                .fill(Color.gray.opacity(0.3))
-                                .frame(width: 200, height: 29)
-                            
-                            RoundedRectangle(cornerRadius: 24)
-                                .fill(context.state.noiseLevel.noiseGradientColor)
-                                .frame(width: CGFloat(context.state.progress) / 100 * 200, height: 29)
-                        }
-                        .padding(.horizontal)
-                        
                         ZStack {
                             RoundedRectangle(cornerRadius: 24)
                                 .fill(Color.clear)
@@ -116,24 +83,71 @@ struct DynamicIslandWidgetLiveActivity: Widget {
                                     RoundedRectangle(cornerRadius: 24)
                                         .stroke(context.state.noiseLevel.noiseBorderGradientColor, lineWidth: 1)
                                 )
-                            HStack(spacing: 1) {
+                            HStack(alignment: .bottom, spacing: 1) {
                                 Text("\(context.state.noiseLevel.emoji)")
+                                    .fixedSize()
+                                    .padding(.bottom,2)
                                 Text("\(context.state.decibels)")
                                     .foregroundStyle(.sgmWhite)
                                     .font(.system(size: 22, weight: .semibold))
+                                    .frame(width: 30)
                                 Text("dB")
                                     .foregroundStyle(.sgmWhite)
                                     .font(.system(size: 16, weight: .regular))
+                                    .padding(.bottom, 2)
                             }
                         }
+                        .padding(.leading, 8)
                     }
+                }
+                
+                // Dynamic Island 우측에 표시되는 영역
+                DynamicIslandExpandedRegion(.trailing) {
+                    
+                    ZStack {
+                        Circle()
+                            .fill(.sgmGray2)
+                            .frame(width: 40, height: 40)
+                        
+                        Button(intent: EndLiveActivityIntent()) {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.sgmGrayA)
+                                .bold()
+                                .padding(14)
+                        }
+                        .frame(width: 40, height: 40)
+                    }
+                    .padding(.trailing, 8)
+                }
+                
+                // Dynamic Island 바로 하단에 표시되는 영역
+                DynamicIslandExpandedRegion(.center) { }
+                
+                // Dynamic Island center 하단에 표시되는 영역
+                // center가 없을 시 center에 위치
+                DynamicIslandExpandedRegion(.bottom) {
+                    GeometryReader { geometry in
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 24)
+                                .fill(Color.gray.opacity(0.3))
+                                .frame(width: geometry.size.width, height: 29)
+                            
+                            RoundedRectangle(cornerRadius: 24)
+                                .fill(context.state.noiseLevel.noiseGradientColor)
+                                .frame(width: geometry.size.width * CGFloat(context.state.progress) / 100, height: 29)
+                        }
+                    }
+                    .padding(.bottom, 18)
+                    .padding(.horizontal, 11)
                 }
             } compactLeading: { // compact Mode에서 좌측에 나타나는 View
                 HStack(alignment: .bottom, spacing: 1) {
                     Text("\(context.state.decibels)")
                         .foregroundStyle(.sgmWhite)
                         .font(.system(size: 16, weight: .semibold))
-                        .frame(width: 25)
+                        .frame(width: 22)
                     Text("dB")
                         .foregroundStyle(.sgmWhite)
                         .font(.system(size: 10, weight: .regular))
