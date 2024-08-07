366fee9ab25b3aaea97466de33370fdb91f25d8a
SANGDOLEE <xkdxh625@naver.com>
2024-08-08 02:22:56 +0900

    Chore: 주석 수정

--- Alright/Alright/Source/Helper/RoundedCorner.swift
+++ Alright/Alright/Source/Helper/RoundedCorner.swift
@@ -19,9 +19,3 @@ struct RoundedCorner: Shape {
         return Path(path.cgPath)
     }
 }
-
-//extension View {
-//    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-//        clipShape(RoundedCorner(radius: radius, corners: corners))
-//    }
-//}
