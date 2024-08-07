ce5080d81249b1d6a1c8bdefe0d1463d26c1ed3d
SANGDOLEE <xkdxh625@naver.com>
2024-08-08 02:18:17 +0900

    Feat: Custom한 도움말SheetView 추가 및 적용

--- Alright/Alright/Source/Helper/RoundedCorner.swift
+++ Alright/Alright/Source/Helper/RoundedCorner.swift
@@ -19,3 +19,9 @@ struct RoundedCorner: Shape {
         return Path(path.cgPath)
     }
 }
+
+//extension View {
+//    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+//        clipShape(RoundedCorner(radius: radius, corners: corners))
+//    }
+//}
