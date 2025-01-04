325ed8b9d2dc52718199812989089b0783841533
SANGDOLEE <xkdxh625@naver.com>
2025-01-04 17:53:55 +0900

    Feat: endLiveActivity() 동기함수로 변경 및 앱종료시 호출

--- Alright/Alright/Source/Application/AppDelegate.swift
+++ Alright/Alright/Source/Application/AppDelegate.swift
@@ -12,5 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // 앱이 종료될때 실행
     func applicationWillTerminate(_ application: UIApplication) {
         print(#function)
+        NoiseMeter.shared.endLiveActivity()
+        print("[앱 강제종료 됨 : endLiveActivity Done]")
     }
 }
