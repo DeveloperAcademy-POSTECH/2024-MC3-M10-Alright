7461560f27ba9d0fc3a0b62d170c1d9203682fde
SANGDOLEE <xkdxh625@naver.com>
2025-01-04 17:04:40 +0900

    Feat: AppDelegate 파일 및 앱종료시 실행함수 추가

---
+++ Alright/Alright/Source/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Alright
+//
+//  Created by 이상도 on 1/4/25.
+//
+
+import SwiftUI
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // 앱이 종료될때 실행
+    func applicationWillTerminate(_ application: UIApplication) {
+        print(#function)
+    }
+}
